918a64ecd71b8be44c931b7c4fde7f6cac36b68e
poneilldev <p.oneill.apc@gmail.com>
2015-05-02 19:12:32 -0600

    delete

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+
+<html>
+    <head>
+        <?php include $_SERVER['DOCUMENT_ROOT']. '/modules/header.php'; ?>
+    </head>
+    <body>
+        HI
+    </body>
+    
+</html>
+
